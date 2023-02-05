ce68090a326ddfd29c8d8818aaabfc12d2a68b4e
Shovi01 <107983873+Shovi01@users.noreply.github.com>
2023-02-05 22:47:13 +0530

    mail change

--- AI jarvis project.py
+++ AI jarvis project.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
 
 
     def sendEmail(to, content):
-        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server = smtplib.SMTP('pk7982.gmail.com', 587)
         server.ehlo()
         server.starttls()
         server.login('youremail@gmail.com', 'your-password')
